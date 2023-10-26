3835174438d671beeaafcc93b6ebe4a9fa8c85b5
maclean362 <146935617+maclean362@users.noreply.github.com>
2023-10-26 15:58:14 +0100

    Update telegram-villa.py - first steps

--- telegram-villa.py
+++ telegram-villa.py
@@ -4,20 +4,24 @@ from telegram.ext import ApplicationBuilder, MessageHandler, filters
 import os
 
 TOKEN = os.getenv("TOKEN")
+APITOKEN = 'JQVl8GNrhQzVhdf7hLVnq1pw6BqvxOGUNhfyBsMm0Ty9H5chvGjYD85m1vhi'
+
+URL = 'https://api.sportsmonks.com/api/v2.0'
+TEAM = 'Aston Villa'
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-async def We(update: Update, context):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Super John Mcginn")
+async def get_team_stats(update: Update, context):
+    await context.bot.send_message(chat_id=update.effective_chat.id, )
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
     
     # Using filters.Text to filter messages by their content
-    start_handler = MessageHandler(filters.Text('Weve got Mcginn'), We)
+    start_handler = MessageHandler(filters.Text()
     application.add_handler(start_handler)
     
     application.run_polling()
