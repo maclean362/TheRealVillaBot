11d8ba559d28ed3eb10d40f285a69d6b8afb8145
Graham Maclean <GrahamM@Daves-MacBook-Pro.local>
2023-11-02 07:27:04 +0000

    Telegram bot with villa players - working api

--- telegram-villa.py
+++ telegram-villa.py
@@ -1,19 +1,38 @@
 import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+import requests
+from telegram import Update, Bot
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+import os
+import json
 
-TOKEN = '6552538823:AAEepUTvV-LEkj0h0Kq5omQuzNk0c0FJEmQ'
+TELEGRAMGRAMTOKEN = os.getenv("TOKEN")
+url = "https://api-football-v1.p.rapidapi.com/v2/players/squad/66/2023-2024"
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+headers = {
+    'x-rapidapi-host' :  "api-football-v1.p.rapidapi.com",
+    'x-rapidapi-key' : "7b8e4fe094msh920a3b9a254e231p1cba58jsnd985b6e2f537"
+}
+
+response = requests.request("GET", url, headers=headers)
+
+playerlist = []
+
+data = json.loads(response.text)
+
+if "api" in data and "players" in data["api"]:
+    for player in data["api"]["players"]:
+        playerlist.append(f"{player['player_name']} - {player['position']}")
+else:
+    print("The response does not have the expected structure.")
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=playerlist)
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(TOKEN).build()
+    application = ApplicationBuilder().token(TELEGRAMGRAMTOKEN).build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
